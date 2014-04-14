8f5e1f91b4f75367395769264dcb959d946bf29b
Jenny H. Rimer <Hitomi@Hitos-Nito-Book-of-Air.local>
2014-04-14 16:16:02 +0200

    Initial Commit

---
+++ HeavensCry_Map_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HeavensCry_Map_1
+//
+//  Created by Jenny H. Rimer on 4/14/14.
+//  Copyright (c) 2014 lovenirds. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
